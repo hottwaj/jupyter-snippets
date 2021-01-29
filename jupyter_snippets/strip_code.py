958475e55b2564fe4588a9935548ac6c2af8fda2
hottwaj <jonathan.a.clarke@gmail.com>
2021-01-29 17:09:58 +0000

    strip_code tweaked for py3, and added css for hiding blank output cells

--- jupyter_snippets/strip_code.py
+++ jupyter_snippets/strip_code.py
@@ -11,8 +11,9 @@ import sys
 
 def strip_code(bs_tree):
     removeds = [n.extract() for n in bs_tree.find_all(class_ = 'input')]
-    removeds = [n.extract() for n in bs_tree.find_all(class_ = 'output_prompt')]    
-
+    removeds = [n.extract() for n in bs_tree.find_all(class_ = 'output_prompt')]
+    removeds = [n.extract() for n in bs_tree.find_all(class_ = 'jp-Cell-inputWrapper')]
+    removeds = [n.extract() for n in bs_tree.find_all(class_ = 'jp-OutputPrompt')]
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -23,22 +24,22 @@ Strip input code cells and input prompts from a Jupyter HTML file so that it's s
 
     parser.add_option('-o', '--output_file', default=[],
                          help='Write output to the given file (default is to write to "cleaned FILENAME").',
-                         dest='output_file', action='append')  
+                         dest='output_file', action='append')
 
     parser.add_option('-i', '--insert_css', default=[],
                          help='Insert custom CSS into the file.',
-                         dest='insert_css', action='append')  
-                                                  
+                         dest='insert_css', action='append')
+
     options, args = parser.parse_args()
-    
+
     if len(args) == 0:
         parser.print_help()
         sys.exit(1)
-        
+
     if len(options.output_file) > 0 and len(options.output_file) != len(args):
         print('ERROR: The number of input files given does not match the number of output files given.', file=sys.stderr)
         sys.exit(1)
-        
+
     import codecs
     for i, filename in enumerate(args):
         if i >= len(options.output_file):
@@ -48,19 +49,19 @@ Strip input code cells and input prompts from a Jupyter HTML file so that it's s
             outfile = os.path.join(*(path + ['cleaned ' + fname]))
         else:
             outfile = options.output_file[i]
-            
+
         doctree = BeautifulSoup(codecs.open(filename, encoding='utf-8'), 'lxml')
 
         strip_code(doctree)
-        
+
         if len(options.insert_css):
             for cssfile in options.insert_css:
-                f = file(cssfile, 'r')
+                f = open(cssfile, 'r')
                 styletag = doctree.new_tag("style")
-                styletag.append(f.read())                
-                doctree.body.insert(-1, styletag)   
+                styletag.append(f.read())
+                doctree.body.insert(-1, styletag)
                 f.close()
-        
+
         f = codecs.open(outfile, encoding='utf-8', mode='w')
         f.write(str(doctree))
-        f.close()        
+        f.close()
